1c756bbd91e17c7342cd5f17f9b8895c66a6f7cd
whatbee <gabe.bf@gmail.com>
2022-12-07 12:26:14 +0000

    Added favicon and changed CSS
    added variable colours in CSS

---
+++ index.js
@@ -0,0 +1,13 @@
+const template = document.createElement('template');
+
+template.innerHTML = `
+<br>
+<ul class="nav">
+  <li><a href="index.html">Home</a></li>
+  <li><a href="askqs.html">Asking Questions</a></li>
+  <li><a href="projects.html">Projects</a></li>
+  <li><a href="links.html">Links</a></li>
+</ul>
+`;
+
+document.body.appendChild(template.content);
