42b0d5258fb08ccc88fd336f1f9011949cc69c56
whatbee <gabe.bf@gmail.com>
2023-04-24 19:44:50 +0100

    add stuff

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ template.innerHTML = `
 <br>
 <ul class="nav">
   <li><a href="index.html">Home</a></li>
-  <li><a href="askqs.html">Asking Questions</a></li>
   <li><a href="projects.html">Projects</a></li>
   <li><a href="links.html">Links</a></li>
 </ul>
