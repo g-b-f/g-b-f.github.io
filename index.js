2da55bcce74a2a111272570b44313bc035d4a7ec
whatbee <gabe.bf@gmail.com>
2023-09-20 23:28:17 +0100

    update links to be relative to root

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const template = document.createElement('template');
 template.innerHTML = `
 <br>
 <ul class="nav">
-  <li><a href="index.html">Home</a></li>
-  <li><a href="projects.html">Projects</a></li>
-  <li><a href="links.html">Links</a></li>
+  <li><a href="/index.html">Home</a></li>
+  <li><a href="/projects.html">Projects</a></li>
+  <li><a href="/links.html">Links</a></li>
 </ul>
 `;
 
