fa06d8d33ecf876ce20ca26a16c5796cc4b6f666
whatbee <gabe.bf@gmail.com>
2023-10-11 19:14:38 +0100

    change make "li" formatting only apply to topLinks

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const template = document.createElement('template');
 template.innerHTML = `
 <br>
 <ul class="nav">
-  <li><a href="/index.html">Home</a></li>
-  <li><a href="/projects.html">Projects</a></li>
-  <li><a href="/links.html">Links</a></li>
-  <li><a href="/themes.html">Themes</a></li>
+  <li class="topLinks"><a href="/index.html">Home</a></li>
+  <li class="topLinks"><a href="/projects.html">Projects</a></li>
+  <li class="topLinks"><a href="/links.html">Links</a></li>
+  <li class="topLinks"><a href="/themes.html">Themes</a></li>
 </ul>
 `;
 
