aeef230a0882c4ea9d08e349f68f73fa05c16dd0
whatbee <gabe.bf@gmail.com>
2026-05-06 17:31:49 +0100

    more wordiply

--- src/pages/WordiplySolver.tsx
+++ src/pages/WordiplySolver.tsx
@@ -1,63 +1,61 @@
 import { useState, type ChangeEvent} from 'react';
+
 export default function WordiplySolver() {
+  const [word, setWord] = useState('');
+  const [error, setError] = useState<Error | null>(null);
+  const [status, setStatus] = useState('typing');
+  const [wordList, setwordList] = useState<string[] | null>(null);
+  const [result, setResult] = useState(['']);
 
+  const err = (e:string) => {throw new Error(e + '!')};
 
-
-const [word, setWord] = useState('');
-const [error, setError] = useState<Error | null>(null);
-const [status, setStatus] = useState('typing');
-const [wordList, setwordList] = useState(['']);
-const [result, setResult] = useState(['']);
-
-const err = (e:string) => setError(Error(e + '!'));
-
-function get_matching_words(match_str: string): string[]{
-  let ret = [];
-  for (const candidate of wordList){
-    if (candidate.includes(match_str)){
-      ret.push(candidate);
+  function get_matching_words(match_str: string): string[]{
+    let ret = [];
+    if (wordList === null) {throw new Error("word list is unexpectedly null")}
+    for (const candidate of wordList){
+      if (candidate.includes(match_str)){
+        ret.push(candidate);
+      }
     }
+    return ret;
+    }
+
+  async function handleSubmit(event: ChangeEvent<HTMLFormElement>) {
+
+      event.preventDefault();
+      setStatus('submitting');
+
+      try {
+        const answer = word.toLowerCase()
+        if (! /^[A-Za-z]+$/.test(answer)){
+
+          err("Must be letters only");
+        }
+        else if (answer.length <2 ){
+          err("Too few letters");
+        }
+        else if (answer.length >5 ){
+          err("Too many letters");
+        }
+        setStatus('success');
+      
+        const fileUrl = "/assets/words.txt";
+        const response = (await fetch(fileUrl))
+        if (!response.ok) err("File not found");
+        const text = await response.text();
+        setwordList(text.split("\n"));
+        setResult(get_matching_words(answer));
+      }
+
+      catch (er: any) {
+        setStatus('typing');
+        setError(er);
+        console.error(er)
+        return
+      }
   }
-  return ret;
-}
 
-async function handleSubmit(event: ChangeEvent<HTMLFormElement>) {
-    event.preventDefault();
-    setStatus('submitting');
-    try {
-      await submitForm(word);
-      setStatus('success');
-    } catch (error: any) {
-      setStatus('typing');
-      setError(error);
-    }
-    const fileUrl = "/assets/words.txt";
-    const response = (await fetch(fileUrl))
-    if (!response.ok) throw new Error("File not found");
-    const text = await response.text();
-    setwordList(text.split("\n"));
-
-    setResult(get_matching_words(word));
-}
-
-function submitForm(answer: string): string {
-    const ret = answer.toLowerCase();
-    if (! /^[A-Za-z]+$/.test(answer)){
-      err("Must be letters only");
-    }
-    else if (answer.length <2 ){
-      err("Too few letters");
-    }
-    else if (answer.length >5 ){
-      err("Too many letters");
-    }
-
-    return ret
-}
-
-const handleTextareaChange = (e: ChangeEvent<HTMLInputElement>) => setWord(e.target.value);
-
-
+  const handleTextareaChange = (e: ChangeEvent<HTMLInputElement>) => setWord(e.target.value);
 
   return (
     <>
