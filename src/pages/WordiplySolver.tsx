da325b2e3858bc69768f0dcbfc92b0ee4d34cf76
whatbee <gabe.bf@gmail.com>
2026-05-09 16:38:32 +0100

    make wordList load aync

--- src/pages/WordiplySolver.tsx
+++ src/pages/WordiplySolver.tsx
@@ -1,31 +1,49 @@
-import { useState, type ChangeEvent, type ReactElement} from 'react';
+import { useState, useRef, type ChangeEvent, type ReactElement} from 'react';
 const fileUrl = "/assets/words.txt";
-
-/* TODO:
-  - Load wordList on first page load, block get_matching_words() until wordList not null
-  - Fade in results to prevent flickering
-*/
 
 export default function WordiplySolver() {
   const [word, setWord] = useState('');
   const [error, setError] = useState<Error | null>(null);
   const [status, setStatus] = useState('typing');
-  // const [wordList, setwordList] = useState<string[] | null>(nulrl);
   const [result, setResult] = useState<string[]>([]);
+  const wordList = useRef<string[] | null>(null);
 
   const err = (e:string) => {throw new Error(e + '!')};
 
+  if (wordList.current === null){
+    console.debug("fetching word list")
+    get_word_list().then((list) => {wordList.current = list}).catch((e) => {
+      console.error("Error fetching word list: " + e);
+      err("Failed to load word list");
+    });
+  }
+
+  async function get_word_list(): Promise<string[]>{
+      const start_time = performance.now();
+      const response = (await fetch(fileUrl))
+      if (!response.ok) err("Internal word list not found");
+      const text = await response.text();
+      const word_list = text.split("\n");
+      const end_time = performance.now();
+      console.debug(
+        "got word list. character count: " + text.length +
+        " word count: " + word_list.length +
+        " took: " + Math.round(end_time - start_time) + " ms"
+      );
+      return word_list;
+  }
+
   function get_matching_words(match_str: string, list: string[]): string[]{
     console.debug("getting matching words for " + match_str)
-    const ret: string[] = [];
+    const matching_words: string[] = [];
     for (const candidate of list){
       if (candidate.includes(match_str)){
-        ret.push(candidate);
+        matching_words.push(candidate);
       }
     }
-    console.log("got " + ret.length + " matching words for " + match_str)
-    ret.sort((a, b) => b.length - a.length);
-    return ret;
+    console.log("got " + matching_words.length + " matching words for " + match_str)
+    matching_words.sort((a, b) => b.length - a.length);
+    return matching_words;
   }
 
   async function handleSubmit(event: ChangeEvent<HTMLFormElement>){
@@ -48,16 +66,8 @@ export default function WordiplySolver() {
       }
     
       console.debug("validated input, fetching word list")
-      const response = (await fetch(fileUrl))
-      if (!response.ok) err("Internal word list not found");
-      const text = await response.text();
-      const words = text.split("\n");
-      console.debug(
-        "got word list. character count: " + text.length +
-        " word count: " + words.length
-      )
-      // setwordList(words);
-      setResult(get_matching_words(answer, words));
+      const list = wordList.current ?? await get_word_list();
+      setResult(get_matching_words(answer, list));
     }
 
     catch (er: any) {
@@ -69,7 +79,7 @@ export default function WordiplySolver() {
     setStatus('success');
     setError(null);
     const end_time = performance.now();
-    console.log("Calulated in " + Math.round(end_time - start_time) + " ms")
+    console.debug("Calulated in " + Math.round(end_time - start_time) + " ms")
   }
 
   const handleTextareaChange = (e: ChangeEvent<HTMLInputElement>) => setWord(e.target.value);
