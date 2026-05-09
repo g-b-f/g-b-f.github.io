9d76a1e826b4d9e75eea59647ae39d79f832e6b4
whatbee <gabe.bf@gmail.com>
2026-05-10 00:57:36 +0100

    Add Portfolio page

---
+++ src/pages/Portfolio.tsx
@@ -0,0 +1,10 @@
+export default function Portfolio() {
+  return (
+    <section id="center">
+      <div>
+        <h1>Portfolio</h1>
+        <p>This page will showcase projects, skills, and work samples.</p>
+      </div>
+    </section>
+  );
+}
