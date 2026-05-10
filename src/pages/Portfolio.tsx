fbb245f6a6767d8db21ac1f54a297b70476e091f
whatbee <gabe.bf@gmail.com>
2026-05-10 02:19:52 +0100

    tidy css

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -3,15 +3,14 @@ import { LoremIpsum } from '../Utils';
 
 export default function Portfolio() {
   return (
-    <section>
-      <div id='intro'>
-        <h1>Gabriel Birkbeck Frazer</h1>
-        <h2>A good programmer, who is brilliant and smart (and made this website)</h2>
-      </div>
-
-    <div id='description'>
-        <p>{new LoremIpsum().ipsum_text}</p>
-    </div>
+    <section className='portfolio'>
+        <section className='hero'>
+            <h1>Gabriel Birkbeck Frazer</h1>
+            <h2>A good programmer, who is brilliant and smart (and made this website)</h2>
+        </section>
+        <section className='intro'>
+            <p>{new LoremIpsum().ipsum_text}</p>
+        </section>
     </section>
   );
 }
