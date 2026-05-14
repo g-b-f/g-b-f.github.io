b12ec6c94befafd197ddc293bfdd8a7b8fd7c820
whatbee <gabe.bf@gmail.com>
2026-05-14 19:21:06 +0100

    add (framer) motion

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,6 +1,19 @@
 import './CSS/Portfolio.css';
 import { DummyImage } from 'react-simple-placeholder-image'
 import { LoremIpsum } from '../Utils';
+import { motion } from "motion/react"
+import type { ReactElement } from 'react';
+
+function fade_in(contents: ReactElement, delay=0.3, duration=0.5): ReactElement{
+    return (
+    <motion.div
+        className='image-text'
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: duration, delay: delay }}>
+        {contents}
+    </motion.div>)
+}
 
 export default function Portfolio() {
     const lorem_ipsum = new LoremIpsum()
@@ -11,24 +24,24 @@ export default function Portfolio() {
             <h2>A good programmer, who is brilliant and smart (and made this website)</h2>
         </section>
         <section className='intro'>
-            <div className='image-text'>
+            {fade_in(<>
                 <p>{lorem_ipsum.first_paragraph}</p>
                 <div className='images'>
                     <DummyImage width={2000} height={2000} shape='image' />
                 </div>
-            </div>
-            <div className='image-text'>
+            </>, 0)}
+            {fade_in(<>
                 <p>{lorem_ipsum.second_paragraph}</p>
                 <div className='images'>
                     <DummyImage width={2000} height={2000} shape='avatar' />
                 </div>
-            </div>
-            <div className='image-text'>
+            </>)}
+            {fade_in(<>
                 <p>{lorem_ipsum.third_paragraph}</p>
                 <div className='images'>
                     <DummyImage width={2000} height={2000} shape='image' />
                 </div>
-            </div>
+            </>)}
         </section>
     </section>
   );
