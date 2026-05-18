c38aa28838868862c09a055d1f5456d8fd9ef5d6
whatbee <gabe.bf@gmail.com>
2026-05-19 00:02:16 +0100

    hide portfolio

--- src/modules/navigation.tsx
+++ src/modules/navigation.tsx
@@ -13,6 +13,6 @@ type Route = {
 export const routes: Route[] = [
   { path: "/", label: "Home", component: lazyPage("Home") },
   { path: "/london", label: "Boroughs Picker", component: lazyPage("LondonBoroughs")},
-  { path: "/portfolio", label: "Portfolio", component: lazyPage("Portfolio") },
+  { path: "/portfolio", label: "Portfolio", component: lazyPage("Portfolio"), showInNav: false },
   { path: "/solver", label: "Wordiply Solver", component: lazyPage("WordiplySolver")},
 ];
