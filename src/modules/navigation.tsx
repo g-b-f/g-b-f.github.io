38b71a4c62d8c59b4cd734c107df551e28303142
whatbee <gabe.bf@gmail.com>
2026-05-18 14:32:22 +0100

    add route type

--- src/modules/navigation.tsx
+++ src/modules/navigation.tsx
@@ -3,9 +3,16 @@ const pages = import.meta.glob('../pages/*.tsx');
 
 const lazyPage = (name: string) => lazy(pages[`../pages/${name}.tsx`] as any);
 
-export const routes = [
+type Route = {
+  path: string;
+  label: string;
+  component: React.ComponentType;
+  showInNav?: boolean;
+};
+
+export const routes: Route[] = [
   { path: "/", label: "Home", component: lazyPage("Home") },
-  { path: "/portfolio", label: "Portfolio", component: lazyPage("Portfolio") },
+  { path: "/portfolio", label: "Portfolio", component: lazyPage("Portfolio"), showInNav: false },
   { path: "/calculator", label: "Calculator", component: lazyPage("Calculator"), showInNav: false },
   { path: "/solver", label: "Wordiply Solver", component: lazyPage("WordiplySolver")},
 ];
