f438bcc2c14bba856ce15199670cfc978773f841
g-b-f <gabe.bf@gmail.com>
2026-05-20 15:32:06 +0100

    fix name issue AGAIN

---
+++ src/modules/Navigation.tsx
@@ -0,0 +1,18 @@
+import { lazy } from 'react';
+const pages = import.meta.glob('../pages/*.tsx');
+
+const lazyLoad = (name: string) => lazy(pages[`../pages/${name}.tsx`] as any);
+
+type RouteType = {
+  path: string;
+  label: string;
+  component: React.ComponentType;
+  showInNav?: boolean;
+};
+
+export const routes: RouteType[] = [
+  { path: "/", label: "Home", component: lazyLoad("Home") },
+  { path: "/london", label: "Boroughs Picker", component: lazyLoad("LondonBoroughs")},
+  { path: "/portfolio", label: "Portfolio", component: lazyLoad("Portfolio"), showInNav: false },
+  { path: "/solver", label: "Wordiply Solver", component: lazyLoad("WordiplySolver")},
+];
