8d6b14f67a5faaddbd957dbebe0b26d77e761665
whatbee <gabe.bf@gmail.com>
2026-05-21 17:31:45 +0100

    visual tweaks

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -3,7 +3,7 @@ import './CSS/Portfolio.css';
 const languages = {
   "Python": 10,
   "Java": 7,
-  "C/ C++": 7,
+  "C/C++": 7,
   "Bash": 8,
   "React": 5
 }
@@ -11,18 +11,23 @@ const languages = {
 function LanguageTable(props: Record<string, number>) {
   const languageEntries = Object.entries(props).sort((a, b) => b[1] - a[1]);
   return (
+  <div className="languages">
+    {/* <h3>Programming languages</h3> */}
     <table>
       <tbody>
         {languageEntries.map(([language, value]) => (
           <tr key={language}>
             <td>{language}</td>
             <td>
-              <progress id={language + "_progress"} value={value} max="10">{value}</progress>
+              <progress
+                id={language + "_progress"} value={value} max="10">{value}
+              </progress>
             </td>
           </tr>
         ))}
       </tbody>
     </table>
+  </div>
   );
 }
 
@@ -46,16 +51,13 @@ export default function Portfolio() {
               and eager to contribute to impactful projects.
             </p>
           </div>
-          <div className="languages">
-            {/* <h3>Programming languages</h3> */}
-            <LanguageTable {...languages} />
-          </div>
+          <LanguageTable {...languages} />
         </section>
         <br></br>
         <i>
           This portfolio is very much a work in progress,
-          but in the meantime you're welcome to look at the mini webapp
-          projects I've made, above.
+          but in the meantime you're welcome to look at the mini webapps
+          I've made, above.
         </i>
     </section>
   );
